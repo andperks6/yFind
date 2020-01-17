444da12ab9ae47405fd36c3575447e70120e7937
Taylor <taylorbakow@gmail.com>
2020-01-17 15:08:22 -0700

    connecting boards to controllers

---
+++ yFind/SearchBuilding.swift
@@ -0,0 +1,16 @@
+//
+//  SearchBuilding.swift
+//  yFind
+//
+//  Created by Taylor Bakow on 1/17/20.
+//  Copyright © 2020 Esri. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchBuilding: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ yFind/SearchRoom.swift
@@ -0,0 +1,17 @@
+//
+//  SearchRoom.swift
+//  yFind
+//
+//  Created by Taylor Bakow on 1/17/20.
+//  Copyright © 2020 Esri. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchRoom: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
