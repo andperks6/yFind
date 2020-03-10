8538a364876cb3e9ba6d93ed6d737568109bf5f2
Taylor <taylorbakow@gmail.com>
2020-03-09 22:46:28 -0600

    shadow ball

--- yFind/ARViewController.swift
+++ yFind/ARViewController.swift
@@ -1,104 +1,36 @@
-//
-//  ARViewController.swift
-//  yFind
-//
-//  Created by Taylor Bakow on 2/25/20.
-//  Copyright © 2020 Esri. All rights reserved.
-//
-
 import UIKit
+import SceneKit
 import ARKit
-import ArcGISToolkit
-import ArcGIS
 
-class ARViewController: UIViewController {
+class ARViewController: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet weak var switchToMap: UIButton!
-    
-    @IBOutlet weak var cameraView: UIView!
-    
-    @IBOutlet weak var backToSearchButton: UIButton!
-    
-    private let arView = ArcGISARView()
-    
-    /// Overlay used to display user-placed graphics.
-    private let graphicsOverlay: AGSGraphicsOverlay = {
-        let overlay = AGSGraphicsOverlay()
-        overlay.sceneProperties = AGSLayerSceneProperties(surfacePlacement: .absolute)
-        return overlay
-    }()
+    @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        arView.locationDataSource = AGSCLLocationDataSource()
-       
-        arView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let shadowColor = UIColor.lightGray.withAlphaComponent(0.5)
-        let shadow = AGSSimpleMarkerSceneSymbol(style: .sphere, color: shadowColor, height: 0.01, width: 0.25, depth: 0.25, anchorPosition: .center)
-        let point = CGPoint(x: (arView.locationDataSource?.locationManager.location?.coordinate.latitude)!, y: (arView.locationDataSource?.locationManager.location?.coordinate.longitude)!)
-        let shadowGraphic = AGSGraphic(geometry: arView.arScreenToLocation(screenPoint: point), symbol: shadow)
-        graphicsOverlay.renderer = AGSSimpleRenderer(symbol: shadow)
-        graphicsOverlay.graphics.add(shadowGraphic)
-        
-        
-        arView.sceneView.graphicsOverlays.add(graphicsOverlay)
-
-        view.addSubview(arView)
-
-        NSLayoutConstraint.activate([
-            arView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            arView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            arView.topAnchor.constraint(equalTo: view.topAnchor),
-            arView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-            ])
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        sceneView.scene = SCNScene()
+        let circleNode = createSphereNode(with: 0.2, color: .gray)
+        circleNode.position = SCNVector3(0, 0, -1) // 1 meter in front of camera
+        sceneView.scene.rootNode.addChildNode(circleNode)
     }
     
-    
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        // Continuous update mode
-        arView.startTracking(.continuous, completion: nil)
-
-        // One-time update mode
-        // arView.startTracking(.initial, completion: nil)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
     }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        arView.stopTracking()
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
     }
-
-    private func configureSceneForAR() {
-        // Create scene with imagery basemap
-        let scene = AGSScene(basemapType: .imagery)
-
-        // Create an elevation source and add it to the scene
-        let elevationSource = AGSArcGISTiledElevationSource(url:
-            URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!)
-        scene.baseSurface?.elevationSources.append(elevationSource)
-
-        // Allow camera to go beneath the surface
-        scene.baseSurface?.navigationConstraint = .none
-
-        // Display the scene
-        arView.sceneView.scene = scene
-
-        // Configure atmosphere and space effect
-        arView.sceneView.atmosphereEffect = .none
+    
+    func createSphereNode(with radius: CGFloat, color: UIColor) -> SCNNode {
+        let geometry = SCNSphere(radius: radius)
+        geometry.firstMaterial?.diffuse.contents = color
+        let sphereNode = SCNNode(geometry: geometry)
+        return sphereNode
     }
-
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if(segue.identifier == "switchToMap"){
-//            let displayVC = segue.destination as! ViewController
-//        }
-//    }
-    
-    
-
 }
-
